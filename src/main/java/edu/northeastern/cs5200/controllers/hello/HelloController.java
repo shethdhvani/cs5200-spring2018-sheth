92d4511d79643dcace747e8d63f24a864d1e1a8c
shethdhvani <sheth.dhv@husky.neu.edu>
2018-02-02 22:11:18 -0500

    Integrating Spring Boot with MySQL

--- src/main/java/edu/northeastern/cs5200/controllers/hello/HelloController.java
+++ src/main/java/edu/northeastern/cs5200/controllers/hello/HelloController.java
@@ -1,5 +1,9 @@
 package edu.northeastern.cs5200.controllers.hello;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +18,28 @@ public class HelloController {
     public HelloObject sayHelloObject() {
         HelloObject obj = new HelloObject("Hello Dhvani Sheth!");
         return obj;
+    }
+    
+    @Autowired
+    HelloRepository helloRepository;
+    
+    @RequestMapping("/api/hello/insert")
+    public HelloObject insertHelloObject() {
+        HelloObject obj = new HelloObject("Hello Dhvani Sheth!");
+        helloRepository.save(obj);
+        return obj;
+    }
+    
+    @RequestMapping("/api/hello/insert/{msg}")
+    public HelloObject insertMessage(@PathVariable("msg") String message) {
+        HelloObject obj = new HelloObject(message);
+        helloRepository.save(obj);
+        return obj;
+    }
+    
+    @RequestMapping("/api/hello/select/all")
+    public List<HelloObject> selectAllHelloObjects() {
+        List<HelloObject> hellos = (List<HelloObject>)helloRepository.findAll();
+        return hellos;
     }
 }
